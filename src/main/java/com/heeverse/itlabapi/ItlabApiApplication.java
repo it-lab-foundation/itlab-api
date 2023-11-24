79341bc051f9a3f849235a98cd5e9b3494f69093
gutenLee <3bnm1128@naver.com>
2023-11-24 18:07:15 +0900

    init

---
+++ src/main/java/com/heeverse/itlabapi/ItlabApiApplication.java
@@ -0,0 +1,13 @@
+package com.heeverse.itlabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItlabApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItlabApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heeverse/itlabapi/ItlabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeverse.itlabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItlabApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
